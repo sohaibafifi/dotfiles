e4fc2a0ecae36bf972ee1896cb370b45dfa7a894
Tom Ryder <tom@sanctum.geek.nz>
2012-09-14 01:38:49 +1200

    Soften alert color

--- awesome/tyr/theme.lua
+++ awesome/tyr/theme.lua
@@ -4,7 +4,7 @@ theme.font          = "sans 8"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
+theme.bg_urgent     = "#dd7777"
 theme.bg_minimize   = "#444444"
 
 theme.fg_normal     = "#aaaaaa"
