4e0baa5a48567b1875cab4242d3a5ff653daaba1
Tom Ryder <tom@sanctum.geek.nz>
2013-06-23 15:48:29 +1200

    Preserve options for cd alias
    Also better explanatory notes

--- bash/bashrc.d/cd.bash
+++ bash/bashrc.d/cd.bash
@@ -1,11 +1,17 @@
-# If given two arguments to cd, replace the first with the second in $PWD
+# If given two arguments to cd, replace the first with the second in $PWD,
+# emulating a Zsh function that I often find useful; preserves options too
 function cd {
+    while getopts lPe opt
+    do
+        local opts="$opts -$opt"
+    done
+    shift $(($OPTIND-1))
     if [[ -n "$2" ]]; then
-        builtin cd "${PWD/$1/$2}"
+        builtin cd $opts "${PWD/$1/$2}"
     elif [[ -n "$1" ]]; then
-        builtin cd "$1"
+        builtin cd $opts "$1"
     else
-        builtin cd
+        builtin cd $opts
     fi
 }
 
