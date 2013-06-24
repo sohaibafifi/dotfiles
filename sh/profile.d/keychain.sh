6c871f4fb093436a6f83039202503402f669bb7b
Tom Ryder <tom@sanctum.geek.nz>
2013-06-24 15:42:53 +1200

    Discard stderr from command calls
    Looks like older versions of Bash emit well-intended error messages if
    the command is not found that newer ones don't

--- sh/profile.d/keychain.sh
+++ sh/profile.d/keychain.sh
@@ -1,6 +1,6 @@
 # Keychain
-command -v ssh-askpass >/dev/null \
+command -v ssh-askpass >/dev/null 2>&1 \
     && export SSH_ASKPASS=`which ssh-askpass`
-command -v keychain >/dev/null \
+command -v keychain >/dev/null 2>&1 \
     && eval `keychain --eval --ignore-missing --quiet id_dsa id_rsa id_ecsda`
 
