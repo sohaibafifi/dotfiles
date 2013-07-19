8167badca115f69889234f5430e7e57d01091af1
Tom Ryder <tom@sanctum.geek.nz>
2013-07-20 00:41:19 +1200

    Move aliases into separate file

---
+++ bash/bashrc.d/aliases.bash
@@ -0,0 +1,15 @@
+# I always want a unified diff
+alias diff='diff -u'
+
+# I actually use ed now and then, go figure
+alias ed='ed -p:'
+
+# Make gdb shut up
+alias gdb='gdb -q'
+
+# Protect innocent MySQL databases from my stupidity
+alias mysql='mysql --safe-updates'
+
+# I always do this, and I hate slow train
+alias sl='ls'
+
