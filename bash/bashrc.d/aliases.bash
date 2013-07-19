78953b1d287e82a141a8f8e4b423c96fcf158941
Tom Ryder <tom@sanctum.geek.nz>
2013-07-20 00:54:01 +1200

    Improve comments for aliases

--- bash/bashrc.d/aliases.bash
+++ bash/bashrc.d/aliases.bash
@@ -1,15 +1,14 @@
-# I always want a unified diff
+# Use a unified format for diff by default
 alias diff='diff -u'
 
-# I actually use ed now and then, go figure
+# Add a colon prompt to ed when a command is expected rather than text; makes
+# it feel a lot more like using ex
 alias ed='ed -p:'
 
-# Make gdb shut up
+# Don't print the GDB copyright message on every invocation
 alias gdb='gdb -q'
 
-# Protect innocent MySQL databases from my stupidity
+# Refuse to do highly destructive things in the MySQL client; prevents me from
+# updating or deleting rows without a WHERE clause, among other things
 alias mysql='mysql --safe-updates'
-
-# I always do this, and I hate slow train
-alias sl='ls'
 
