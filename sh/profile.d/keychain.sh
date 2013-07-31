bd81f51d901deeb42f5ce266faff32db7b5c7f2c
Tom Ryder <tom@sanctum.geek.nz>
2013-07-31 17:39:50 +1200

    Don't use backticks for command substitution
    I thought the $( ) syntax wasn't defined by POSIX, but it is, and it
    works in basically every modern Bourne shell including dash and ksh. As
    it's much more readable and easier to nest I'll prefer it here.

--- sh/profile.d/keychain.sh
+++ sh/profile.d/keychain.sh
@@ -1,9 +1,9 @@
 # Keychain
 if command -v ssh-askpass >/dev/null; then
-    SSH_ASKPASS=`which ssh-askpass`
+    SSH_ASKPASS=$(which ssh-askpass)
     export SSH_ASKPASS
 fi
 if command -v keychain >/dev/null; then
-    eval `keychain --eval --ignore-missing --quiet id_dsa id_rsa id_ecsda`
+    eval $(keychain --eval --ignore-missing --quiet id_dsa id_rsa id_ecsda)
 fi
 
