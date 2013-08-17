e8b5b77e3ecd2b35bb3d310b75ecca7c0830d562
Tom Ryder <tom@sanctum.geek.nz>
2013-08-18 01:14:53 +1200

    Practicing writing half-decent Bash
    Use array variable to collect options (appending with a pre-3.01
    compatible syntax)

--- bash/bashrc.d/cd.bash
+++ bash/bashrc.d/cd.bash
@@ -1,20 +1,20 @@
 # If given two arguments to cd, replace the first with the second in $PWD,
 # emulating a Zsh function that I often find useful; preserves options too
 __cd() {
-    local opts=
+    local -a opts
     while getopts elP opt; do
-        opts="$opts -$opt"
+        opts["${#opts[@]}"]="-$opt"
     done
     shift "$(($OPTIND-1))"
     if [[ "$#" -eq 2 ]]; then
         if [[ "$PWD" == *"$1"* ]]; then
-            builtin cd $opts "${PWD/$1/$2}"
+            builtin cd "${opts[@]}" "${PWD/$1/$2}"
         else
             printf 'bash: cd: could not replace substring\n'
             return 1
         fi
     else
-        builtin cd $opts "$@"
+        builtin cd "${opts[@]}" "$@"
     fi
 }
 alias cd='__cd'
