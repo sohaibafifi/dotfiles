7132bc0e09a5b9ff0ce690581d03cadf3cdf951f
Tom Ryder <tom@sanctum.geek.nz>
2013-08-18 23:45:07 +1200

    Move --color option for grep to end of alias

--- bash/bashrc.d/grep.bash
+++ bash/bashrc.d/grep.bash
@@ -10,13 +10,6 @@ __grepopts() {
     # Add option to ignore binary files
     grepopts[${#grepopts[@]}]='-I'
 
-    # If the --color option is available and we have a terminal that supports
-    # at least eight colors, add --color=auto to the options
-    local -i colors="$(tput colors)"
-    if [[ "$grephelp" == *--color* ]] && ((colors >= 8)); then
-        grepopts[${#grepopts[@]}]='--color=auto'
-    fi
-
     # If the --exclude option is available, exclude some VCS files
     if [[ "$grephelp" == *--exclude* ]]; then
         grepopts[${#grepopts[@]}]='--exclude=.git{,ignore,modules}'
@@ -25,6 +18,13 @@ __grepopts() {
     # If the --exclude option is available, exclude some VCS dirs
     if [[ "$grephelp" == *--exclude-dir* ]]; then
         grepopts[${#grepopts[@]}]='--exclude-dir=.{cvs,git,hg,svn}'
+    fi
+
+    # If the --color option is available and we have a terminal that supports
+    # at least eight colors, add --color=auto to the options
+    local -i colors="$(tput colors)"
+    if [[ "$grephelp" == *--color* ]] && ((colors >= 8)); then
+        grepopts[${#grepopts[@]}]='--color=auto'
     fi
 
     # Print the options as a single string, space-delimited
