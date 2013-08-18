36739d4897f8c213962162b51ae566cf9c788def
Tom Ryder <tom@sanctum.geek.nz>
2013-08-18 23:44:02 +1200

    Add some comments to the ls/grep alias functions

--- bash/bashrc.d/grep.bash
+++ bash/bashrc.d/grep.bash
@@ -1,18 +1,33 @@
 # Function returns calculated options for grep
 __grepopts() {
+
+    # Declare options array
     local -a grepopts
+
+    # Snarf the output of `grep --help` into a variable
     local grephelp="$(grep --help 2>/dev/null)"
+
+    # Add option to ignore binary files
     grepopts[${#grepopts[@]}]='-I'
+
+    # If the --color option is available and we have a terminal that supports
+    # at least eight colors, add --color=auto to the options
     local -i colors="$(tput colors)"
     if [[ "$grephelp" == *--color* ]] && ((colors >= 8)); then
         grepopts[${#grepopts[@]}]='--color=auto'
     fi
+
+    # If the --exclude option is available, exclude some VCS files
     if [[ "$grephelp" == *--exclude* ]]; then
         grepopts[${#grepopts[@]}]='--exclude=.git{,ignore,modules}'
     fi
+
+    # If the --exclude option is available, exclude some VCS dirs
     if [[ "$grephelp" == *--exclude-dir* ]]; then
         grepopts[${#grepopts[@]}]='--exclude-dir=.{cvs,git,hg,svn}'
     fi
+
+    # Print the options as a single string, space-delimited
     printf -- "${grepopts[*]}"
 }
 
--- bash/bashrc.d/ls.bash
+++ bash/bashrc.d/ls.bash
@@ -1,11 +1,20 @@
 # Function returns calculated options for ls
 __lsopts() {
+
+    # Declare options array
     local -a lsopts
+
+    # Snarf the output of `ls --help` into a variable
     local lshelp="$(ls --help 2>/dev/null)"
+
+    # If the --color option is available and we have a terminal that supports
+    # at least eight colors, add --color=auto to the options
     local -i colors="$(tput colors)"
     if [[ "$lshelp" == *--color* ]] && ((colors >= 8)); then
         lsopts[${#lsopts[@]}]='--color=auto'
     fi
+
+    # Print the options as a single string, space-delimited
     printf -- "${lsopts[*]}"
 }
 
