700ef81fdba7c9ac738e94ff1bb2683730eca211
Tom Ryder <tom@sanctum.geek.nz>
2013-08-25 03:29:06 +1200

    Restructure tmux wrapper function slightly

--- bash/bashrc.d/tmux.bash
+++ bash/bashrc.d/tmux.bash
@@ -8,8 +8,8 @@ tmux() {
     # If no arguments, attempt to attach to an existing session; if that fails,
     # run the tmux binary with no arguments, which by default creates a new
     # session
-    else
-        command tmux attach -d &>/dev/null || command tmux
+    elif ! command tmux attach -d &>/dev/null; then
+        command tmux
     fi
 }
 
