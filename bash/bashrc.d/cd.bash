635eded956682dc3764219e4d4e3c40b9ef69539
Tom Ryder <tom@sanctum.geek.nz>
2013-08-25 22:24:04 +1200

    Use single quotes for printf

--- bash/bashrc.d/cd.bash
+++ bash/bashrc.d/cd.bash
@@ -11,7 +11,7 @@ cd() {
         if [[ $PWD == *"$1"* ]]; then
             builtin cd "${opts[@]}" "${PWD/$1/$2}"
         else
-            printf %s "bash: cd: could not replace substring\n" >&2
+            printf %s 'bash: cd: could not replace substring\n' >&2
             return 1
         fi
     else
