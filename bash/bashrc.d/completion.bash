d0abd9f1ee8bc5f74b2dfe50a21f558eb7b5e423
Tom Ryder <tom@sanctum.geek.nz>
2013-09-01 00:04:10 +1200

    Move SSH completion into its own file

--- bash/bashrc.d/completion.bash
+++ bash/bashrc.d/completion.bash
@@ -22,34 +22,3 @@ complete -c sudo
 # unset with shell variables and functions
 complete -v -A function unset
 
-# ssh/sftp/ssh-copy-id with config hostnames
-_ssh() {
-    local word config hosts option value
-    word=${COMP_WORDS[COMP_CWORD]}
-    config=$HOME/.ssh/config
-    hosts=()
-
-    # Bail if the configuration file is illegible
-    if [[ ! -r $config ]]; then
-        return 1
-    fi
-
-    # Read hostnames from the file, no asterisks
-    while read -r option value _; do
-        if [[ $option == Host && $value != *'*'* ]]; then
-            hosts[${#hosts[@]}]=$value
-        fi
-    done < "$config"
-
-    # Generate completion reply
-    COMPREPLY=( $(compgen -W "${hosts[*]}" -- "$word") )
-}
-complete -F _ssh ssh sftp ssh-copy-id
-
-# scp/rsync with local files and hostnames (colon suffixes)
-_scp() {
-    _ssh
-    COMPREPLY=( "${COMPREPLY[@]/%/:}" )
-}
-complete -f -F _scp scp
-
---
+++ bash/bashrc.d/ssh.bash
@@ -0,0 +1,31 @@
+# Commpletion for ssh/sftp/ssh-copy-id with config hostnames
+_ssh() {
+    local word config hosts option value
+    word=${COMP_WORDS[COMP_CWORD]}
+    config=$HOME/.ssh/config
+    hosts=()
+
+    # Bail if the configuration file is illegible
+    if [[ ! -r $config ]]; then
+        return 1
+    fi
+
+    # Read hostnames from the file, no asterisks
+    while read -r option value _; do
+        if [[ $option == Host && $value != *'*'* ]]; then
+            hosts[${#hosts[@]}]=$value
+        fi
+    done < "$config"
+
+    # Generate completion reply
+    COMPREPLY=( $(compgen -W "${hosts[*]}" -- "$word") )
+}
+complete -F _ssh ssh sftp ssh-copy-id
+
+# Completion for scp/rsync with local files and hostnames (colon suffixes)
+_scp() {
+    _ssh
+    COMPREPLY=( "${COMPREPLY[@]/%/:}" )
+}
+complete -f -F _scp scp
+
