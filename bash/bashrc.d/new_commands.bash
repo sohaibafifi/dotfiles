d6722317510152c41ab1d4ee908ecf006c1aca19
Sohaib Afifi <me@sohaibafifi.com>
2013-09-09 21:39:46 +0100

    NEW - add some bash aliases

---
+++ bash/bashrc.d/new_commands.bash
@@ -0,0 +1,20 @@
+## Modified commands ## {{{
+alias diff='colordiff'              # requires colordiff package
+alias grep='grep --color=auto'
+alias more='less'
+alias df='df -h'
+alias du='du -c -h'
+alias mkdir='mkdir -p -v'
+alias nano='nano -w'
+alias ping='ping -c 5'
+# }}}
+
+## New commands ## {{{
+alias da='date "+%A, %B %d, %Y [%T]"'
+alias du1='du --max-depth=1'
+alias hist='history | grep'         # requires an argument
+alias openports='ss --all --numeric --processes --ipv4 --ipv6'
+alias pgg='ps -Af | grep'           # requires an argument
+alias ..='cd ..'
+# }}}
+
