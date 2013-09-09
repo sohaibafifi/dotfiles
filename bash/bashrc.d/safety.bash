5104f05131c7c51dc17b3dc570fb23e6bfb2b05c
Sohaib Afifi <me@sohaibafifi.com>
2013-09-09 21:40:51 +0100

    NEW - add safety check for some dangerous bash commands

---
+++ bash/bashrc.d/safety.bash
@@ -0,0 +1,13 @@
+## Safety features ## {{{
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -I'                    # 'rm -i' prompts for every file
+# safer alternative w/ timeout, not stored in history
+alias rm=' timeout 3 rm -Iv --one-file-system'
+alias ln='ln -i'
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
+alias cls=' echo -ne "\033c"'       # clear screen for real (it does not work in Terminology)
+# }}}
+
